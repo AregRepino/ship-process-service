b49634b99a2bf148abb65577edd52852443f2a32
areg <test@gmail.com>
2023-02-18 11:06:52 +0300

    fix

--- src/main/java/ru/gumrf/ship/service/ShipService.java
+++ src/main/java/ru/gumrf/ship/service/ShipService.java
@@ -1,8 +1,8 @@
 package ru.gumrf.ship.service;
 
 
-import ru.gumrf.ship.model.HoldProcessTime;
 import org.springframework.stereotype.Service;
+import ru.gumrf.ship.model.HoldProcessTime;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -56,49 +56,6 @@ public class ShipService {
     }
 
 
-//    public List<Double[]> optimizeLoad(List<Double[]> list) {
-//        List<Double[]> copyList = new ArrayList<>(list);
-//        List<Double[]> unloadingList = new ArrayList<>();
-//        List<Double[]> cleaningList = new ArrayList<>();
-//
-//
-//        int N = copyList.size();
-//
-//        for (int i = 0; i < N; i++) {
-//
-//            int unloadingIndex = minUnloading(copyList);
-//
-//            Double minUnloadingTime = copyList.get(unloadingIndex)[0];
-//
-//            int cleaningIndex = minCleaning(copyList);
-//            Double minCleaningTime = copyList.get(cleaningIndex)[1];
-//
-//            if (minUnloadingTime <= minCleaningTime) {
-//                Double[] element = copyList.get(unloadingIndex);
-//                unloadingList.add(element);
-//                copyList.remove(unloadingIndex);
-//
-//            } else {
-//                Double[] element = copyList.get(cleaningIndex);
-//                cleaningList.add(element);
-//                copyList.remove(cleaningIndex);
-//
-//            }
-//
-//
-//        }
-//
-//        List<Double[]> result = new ArrayList<>();
-//
-//
-//        result.addAll(unloadingList);
-//        Collections.reverse(cleaningList);
-//        result.addAll(cleaningList);
-//
-//        return result;
-//
-//    }
-
     public int minUnloading(List<HoldProcessTime> list) {
 
         double min = list.get(0).getUnloadingTime();
@@ -143,47 +100,4 @@ public class ShipService {
 
     }
 
-//    public int minUnloading(List<Double[]> list) {
-//
-//        Double min = list.get(0)[0];
-//
-//        int index = 0;
-//
-//        for (int i = 0; i < list.size(); i++) {
-//
-//            Double time = list.get(i)[0];
-//
-//            if (time < min) {
-//                min = time;
-//                index = i;
-//
-//            }
-//        }
-//
-//        return index;
-//
-//    }
-//
-//    public int minCleaning(List<Double[]> list) {
-//
-//
-//        Double min = list.get(0)[1];
-//
-//        int index = 0;
-//
-//        for (int i = 0; i < list.size(); i++) {
-//
-//            Double time = list.get(i)[1];
-//
-//            if (time < min) {
-//                min = time;
-//                index = i;
-//
-//            }
-//        }
-//
-//
-//        return index;
-//
-//    }
 }
